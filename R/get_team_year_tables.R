ca54e6c37cc4074a7b010971512e182df76a0693
Fabian <fabiandagnone@gmail.com>
2019-03-14 19:54:06 +0800

    add function to get year tables

--- R/get_team_year_tables.R
+++ R/get_team_year_tables.R
@@ -1,4 +1,13 @@
+library(RCurl)
 
 get_team_year_tables <- function(team, year){
+
+  t = which(teams == team)
+
+  url <- paste0("https://afltables.com/afl/stats/teams/",site_teams[t],"/",year,"_gbg.html")
+
+  webpage = read_html(url)
+
+  return(webpage)
 
 }
